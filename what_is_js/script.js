b993e9d1c587734e05c3759ef0d4ec59dc770b5c
Bijirita <bijirita009@gmail.com>
2018-09-25 14:00:09 -0400

    javascripting

---
+++ what_is_js/script.js
@@ -0,0 +1,21 @@
+// function createParagraph() {
+//     var para = document.createElement('p');
+//     para.textContent = 'You clicked the button!';
+//     document.body.appendChild(para);
+//   }
+  
+//   var buttons = document.querySelectorAll('button');
+  
+//   for(var i = 0; i < buttons.length ; i++) {
+//     buttons[i].addEventListener('click', createParagraph);
+//   }
+
+
+
+console.log(var myInt = 5;
+  var myFloat = 6.667;
+  myInt;
+  myFloat;
+  var myNum = -1
+  var myFavnum = 7
+  typeof myInt;)
