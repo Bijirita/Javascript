df2078bd3bc6292523b98be385fc3c183898bd37
Bijirita <bijirita009@gmail.com>
2018-09-26 09:58:33 -0400

    dommanipulation

--- what_is_js/script.js
+++ what_is_js/script.js
@@ -12,10 +12,39 @@
 
 
 
-console.log(var myInt = 5;
-  var myFloat = 6.667;
-  myInt;
-  myFloat;
-  var myNum = -1
-  var myFavnum = 7
-  typeof myInt;)
+// console.log("hola")
+
+//   var myFloat = 6.667;
+//   myInt;
+//   myFloat;
+//   var myNum = -1
+//   var myFavnum = 7
+//   typeof myInt;
+
+//   console(myFloat);
+
+// var myArray = ['Manchester', 'London', 'Liverpool', 'Birmingham', 'Leeds', 'Carlisle'];
+// myArray.push('Cardiff');
+// var newLength = myArray.push('Bristol');
+// console.log(myArray.length);
+// console.log(myArray);
+// var removedItem = myArray.pop();
+// console.log(myArray);
+// console.log(removedItem);
+// var myNewString = myArray.join(',');
+// console.log(myNewString);
+
+
+// console.log(myArray);
+
+// var myNewString = myArray.join(',');
+// console.log(myNewString);
+
+var myDate = '19' + '67'
+console.log(typeof myDate);
+var myNum = Number(myDate);
+console.log(myNum);
+console.log(typeof myNum);
+ var newString = 'Front' + 242;
+ console.log(newString);
+ console.log(typeof newString);
